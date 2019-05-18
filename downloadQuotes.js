35d7a701419597dedaf031ba96519172fc74417b
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-05-18 13:40:41 +0530

    added vega

--- downloadQuotes.js
+++ downloadQuotes.js
@@ -1,39 +1,54 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const Papa = require('papaparse');
-const url = (symbol) => `https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=${symbol}&segmentLink=3&symbolCount=2&series=EQ&dateRange=1month&fromDate=&toDate=&dataType=PRICEVOLUMEDELIVERABLE`;
-const [,,symbol] = process.argv;
-const downloadHtml = (symbol,onHtml)=>{
-  request({
-    url:url(symbol),
-    headers:{
-      'user-agent': 'Mozilla 5.0'
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const Papa = require("papaparse");
+const url = symbol =>
+  `https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=${symbol}&segmentLink=3&symbolCount=2&series=EQ&dateRange=3month&fromDate=&toDate=&dataType=PRICEVOLUMEDELIVERABLE`;
+const [, , symbol] = process.argv;
+const downloadHtml = (symbol, onHtml) => {
+  console.log("something ");
+  request(
+    {
+      url: url(symbol),
+      headers: {
+        "user-agent": "Mozilla 5.0"
+      }
+    },
+    (err, res, body) => {
+      onHtml(body);
     }
-  },(err,res,body)=>{
-    onHtml(body);
+  );
+};
+
+const readQuotes = function(text) {
+  fs.writeFileSync(`${symbol}.html`, text);
+  main();
+};
+
+downloadHtml(symbol, readQuotes);
+
+const main = function() {
+  const html = fs.readFileSync(`${symbol}.html`, "utf8");
+  const $ = cheerio.load(html);
+  const quotes = [];
+  const names = [];
+  $("table tbody tr th").each(function() {
+    names.push(
+      $(this)
+        .text()
+        .trim()
+    );
   });
-}
 
-//downloadHtml(symbol,readQuotes);
-const html = fs.readFileSync('reliance.html','utf8');
-const $ = cheerio.load(html);
-const quotes = [];
-const names = [];
-$('table tbody tr th').each(function(){
-  names.push($(this).text().trim());
-});
-//console.log(names);
-
-$('table tbody tr:has(td)').each(function(){
-  const tr = this;
-  const quote = {};
-  $('td',tr).each(function(index){
-    const td = this;
-    quote[names[index]] = $(td).text();
-  })
-  quotes.push(quote);
-});
-console.log(quotes);
-const text = Papa.unparse(quotes);
-console.log(text);
+  $("table tbody tr:has(td)").each(function() {
+    const tr = this;
+    const quote = {};
+    $("td", tr).each(function(index) {
+      const td = this;
+      quote[names[index]] = $(td).text();
+    });
+    quotes.push(quote);
+  });
+  const text = Papa.unparse(quotes);
+  fs.writeFileSync(`${symbol}.txt`, text);
+};
--- showChart.js
+++ showChart.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
-const _ = require('lodash');
-const Papa = require('papaparse');
-const asciichart = require ('asciichart')
-const [,,fileName,columnName] = process.argv;
-const column = columnName ||' Close Price';
-const text = fs.readFileSync(fileName,'utf8').trim();
-const quotes = Papa.parse(text,{header:true,dynamicTyping:true}).data;
-const values = _.map(quotes,column);
-const chart = asciichart.plot(values,{height:40});
-console.log(chart);
-console.log(_.map(quotes,q=>`${q.Date} ${q[column]}`).join('\n'));
+const fs = require("fs");
+const _ = require("lodash");
+const vega = require("vega");
+const Papa = require("papaparse");
+const asciichart = require("asciichart");
+const [, , fileName, columnName] = process.argv;
+const column = columnName || "Close Price";
+const text = fs.readFileSync(fileName, "utf8").trim();
+
+const quotes = Papa.parse(text, { header: true, dynamicTyping: true }).data;
+let values = _.map(quotes, column);
+values = values.map(x => +x.split(",").join(""));
+const chart = asciichart.plot(values, { height: 40 });
+console.log(JSON.stringify(quotes));
+fs.writeFileSync("data.json",JSON.stringify(quotes))
+console.log(_.map(quotes, q => `${q.Date} ${q[column]}`).join("\n"));
